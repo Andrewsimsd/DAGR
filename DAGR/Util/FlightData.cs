655865453b7e710d2e926d4e9e8f78bd462a6367
multiplex55 <multiplex55@yahoo.com>
2021-04-11 17:41:54 -0400

    Proposed Initial changes
    Changed the name of "DashBoard" to "DAGR"
    Changed the name of solution from "DashBoard" to "DAGR"
    Changed the namespace from "DashBoard" to "DAGR"
    Added CSVHelper library
    Added DAGRCommon library for future expansion and project wide accessibility
    Removed Reading of the data file from FlightData class
    Changed FlightData class into an instance of a flight data file (better expansion later on)
    Added multiple constructors to FlightData
    Added DAGRReader class to handle potential multiple data files later on
    Added color constants to DAGRCommon for backgroundcolor 1 and 2 (name placeholders for now)
    Moved chart initializations into another method for readability in main Dashboard Form
    Added Dialog

---
+++ DAGR/Util/FlightData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration.Attributes;
+
+namespace DataUtil
+{
+    public class FlightData
+    {
+        /// <summary>
+        /// Data members
+        /// </summary>
+        [Name("altitude")]
+        public float Altitude { get; set; }
+
+        [Name("pitch")]
+        public float Pitch { get; set; }
+
+        [Name("roll")]
+        public float Roll { get; set; }
+
+        [Name("yaw")]
+        public float Yaw { get; set; }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public FlightData()
+        {
+            Altitude = 0;
+            Pitch = 0;
+            Roll = 0;
+            Yaw = 0;
+        }
+        /// <summary>
+        /// String constructor
+        /// </summary>
+        public FlightData(string alt, string pit, string roll, string yaw)
+        {
+            Altitude = float.Parse(alt);
+            Pitch = float.Parse(pit);
+            Roll = float.Parse(roll);
+            Yaw = float.Parse(yaw);
+        }
+        /// <summary>
+        /// Float Converted constructor
+        /// </summary>
+        public FlightData(float alt, float pit, float roll, float yaw)
+        {
+            Altitude = alt;
+            Pitch = pit;
+            Roll = roll;
+            Yaw = yaw;
+        }
+
+        public override string ToString() 
+        {
+            return $"Altitude:{Altitude}, Pitch:{Pitch}, Roll:{Roll}, Yaw:{Yaw}";
+        }
+       
+    }
+}
