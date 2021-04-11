9b41f54f052ce4d982326d1a70190f4c0b175100
Andrewsimsd <andrew.sims.d@gmail.com>
2021-04-11 14:05:39 -0400

    Revert "Delete Dashboard directory"
    This reverts commit 3d38e67ceef9d4b783a3ba8201a18e9a17ea3d02.

---
+++ Dashboard/Util/FlightData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DataUtil
+{
+    public class FlightData
+    {
+        /// <summary>
+        /// Data memebrs
+        /// </summary>
+        public List<float> altitude;
+        public List<float> pitch;
+        public List<float> roll;
+        public List<float> yaw;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public FlightData()
+        {
+            altitude = new List<float>();
+            pitch = new List<float>();
+            roll = new List<float>();
+            yaw = new List<float>();
+        }
+        public void ReadCsv(string filePath)
+        {
+            StreamReader reader = null;
+            if (File.Exists(filePath))
+            {
+                reader = new StreamReader(File.OpenRead(filePath));
+                string headerLine = reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    this.altitude.Add(float.Parse(values[0]));
+                    this.pitch.Add(float.Parse(values[1]));
+                    this.roll.Add(float.Parse(values[2]));
+                    this.yaw.Add(float.Parse(values[3]));
+                }
+            }
+            else
+            {
+                Console.WriteLine("File doesn't exist");
+            }
+        }
+    }
+}
